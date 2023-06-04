7b590dee6a6501a4e3b3fb1a6d457de196e7e235
Adam Goldfine <tulaneadam21@gmail.com>
2023-06-04 18:08:00 -0400

    refactored code to be more testable

---
+++ backupWebsite.js
@@ -0,0 +1,8 @@
+// backupWebsite.js
+const { spawn } = require('child_process');
+
+function backupWebsite(url, frequency) {
+    // ...
+}
+
+module.exports = { backupWebsite };
---
+++ backupWebsite.test.js
@@ -0,0 +1,5 @@
+const { backupWebsite } = require('./backupWebsite');
+
+test('backupWebsite function is defined', () => {
+    expect(backupWebsite).toBeDefined();
+});
